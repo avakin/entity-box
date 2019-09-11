a646030b241d0714a57ef47c9426f3f39f15ce39
Sergey <sergey@sinitic.ai>
2019-09-11 16:24:47 +0300

    tests

--- src/components/EntityBox/EntityBox.test.js
+++ src/components/EntityBox/EntityBox.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import EntityBox from "./EntityBox";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import OnlineStatus from "../OnlineStatus/OnlineStatus";
+import Counter from "../Counter";
 Enzyme.configure({ adapter: new Adapter() });
 
 function setup() {
@@ -13,7 +15,6 @@ function setup() {
   };
 
   const enzymeWrapper = shallow(<EntityBox {...props} />);
-
   return {
     props,
     enzymeWrapper
@@ -25,5 +26,17 @@ describe("EntityBox rendering", () => {
     const { enzymeWrapper, props } = setup();
     expect(enzymeWrapper.find("div.type").text()).toBe(props.entityType);
     expect(enzymeWrapper.find("div.name").text()).toBe(props.entityName);
+    expect(enzymeWrapper.find(OnlineStatus).prop("isConnected")).toBe(
+      props.connected
+    );
+    expect(enzymeWrapper.find(Counter).prop("count")).toBe(props.count);
+  });
+  it("check getting correct props", () => {
+    const { props } = setup();
+    const wrapper = mount(<EntityBox {...props} />);
+    expect(wrapper.props().count).toBe(2);
+    expect(wrapper.props().connected).toBe(false);
+    expect(wrapper.props().entityName).toBe("My entity");
+    expect(wrapper.props().entityType).toBe("Some");
   });
 });
